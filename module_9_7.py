6ece566336ea0a0d14adcbb84c0a22bf72950df3
Anatoliy_Yavorskiy <yardbirds1957@gmail.com>
2024-11-23 15:26:00 +0300

    функциональное программирование

--- module_9_7.py
+++ module_9_7.py
@@ -4,23 +4,34 @@ def is_prime(func):
     """
     def wrapper(*args):
         result_func = func(*args)
-        for i in range(2, result_func):
+        for i in range(2, result_func + 1):
             if result_func % i == 0:
                 prime_check = f"{result_func} - 'составное' число."
             else:
                 prime_check = f"{result_func} - 'простое' число."
             return prime_check
+
     return wrapper
 
 
 @is_prime
 def sum_three(one, two, three):
     """Складывает три числа.
-
     """
     return one + two + three
 
 
+s = sum_three(1, 1, 1)
+print(s)
 
-result = sum_three(2, 3, 6)
-print(result)
+s2 = sum_three(2, 2, 2)
+print(s2)
+
+s3 = sum_three(1, 5, 2)
+print(s3)
+
+s4 = sum_three(1, 5, 5)
+print(s4)
+
+s5 = sum_three(5, 5, 5)
+print(s5)
